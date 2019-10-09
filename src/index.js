3357f29960711dfd6843817eda1ce14d2e338c88
AlexLysikov <alex.lysikov.82@gmail.com>
2019-10-09 19:05:24 +0300

    feat:task complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
 module.exports = function zeros(expression) {
-  // your solution
+  let result = 0;
+  let temp = [];
+  let deuces = false;
+  let factor = expression.split('*');
+
+  for (let item of factor) {
+    if (item[item.length - 2] === '!') {
+      for (let i = parseInt(item); i >= 1; i-= 2) {
+        temp.push(i);
+      }
+    } else {
+        for (let i = parseInt(item); i >= 1; i--) {
+          temp.push(i);
+        }
+    }
+  }
+
+  for (let i = 0; i <temp.length; i++) {
+    if (temp[i] % 5 === 0 && temp[i] / 5 !== 1) {
+      temp.push(temp[i] / 5);
+      temp[i] = 5;
+      } else if (temp[i] % 2 === 0) {
+      deuces = true;
+    }
+  }
+
+  for (let item of temp) {
+    if (item === 5) {
+      if (deuces) {
+        result++
+      }
+    } 
+  }
+
+  return result;
 }
